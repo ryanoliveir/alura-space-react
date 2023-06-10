c3f43d7f93d980dbcc4f0fe3eba9eba565270796
ryanoliveir <ryandehallo229@gmail.com>
2023-06-09 22:05:10 -0300

    :construction: Added Cards in Gallery

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,37 @@
+import styles from './Card.module.scss'
+import { AiOutlineHeart } from 'react-icons/ai'
+import { IoMdResize } from 'react-icons/io'
+
+
+const iconsProps = {
+    size: 24,
+    color: "#d9d9d9"
+
+}
+
+const Card = ({pictureName, imagePATH, credits}) => {
+    return (
+        <div className={styles.card}>
+            <img 
+                className={styles.card__image} 
+                src={imagePATH} 
+                alt={pictureName} 
+            />
+            <p className={styles.card__description}>
+                {pictureName}
+            </p>
+            <div className={styles.card__credits}>
+                <p>{credits}</p>
+                <span>
+                    <AiOutlineHeart {...iconsProps} />
+                    <IoMdResize {...iconsProps} />
+                </span>
+            </div>
+        </div>
+    );
+}
+
+
+export default Card;
+
+
--- src/components/Gallery/index.jsx
+++ src/components/Gallery/index.jsx
@@ -1,11 +1,101 @@
 import styles from './Gallery.module.scss'
 import Tags from  '../Tags';
+import Card from '../Card';
+
+
+const galleryImages = [
+    {
+      "title": "Nome da Foto",
+      "credits": "Alura Space",
+      "image": "/assets/images/gallery/foto-1.png",
+      "id": "1",
+      "tag": "Galáxias"
+    },
+    {
+      "title": "Nome da Foto",
+      "credits": "Alura Space",
+      "image": "/assets/images/gallery/foto-2.png",
+      "id": "2",
+      "tag": "Estrelas"
+    },
+    {
+      "title": "Nome da Foto",
+      "credits": "Alura Space",
+      "image": "/assets/images/gallery/foto-3.png",
+      "id": "3",
+      "tag": "Lua"
+    },
+    {
+      "title": "Nome da Foto",
+      "credits": "Alura Space",
+      "image": "/assets/images/gallery/foto-4.png",
+      "id": "4",
+      "tag": "Galáxias"
+    },
+    {
+      "title": "Nome da Foto",
+      "credits": "Alura Space",
+      "image": "/assets/images/gallery/foto-5.png",
+      "id": "5",
+      "tag": "Estrelas"
+    },
+    {
+      "title": "Nome da Foto",
+      "credits": "Alura Space",
+      "image": "/assets/images/gallery/foto-6.png",
+      "id": "6",
+      "tag": "Nebulosas"
+    },
+    {
+      "title": "Nome da Foto",
+      "credits": "Alura Space",
+      "image": "/assets/images/gallery/foto-7.png",
+      "id": "7",
+      "tag": "Galáxias"
+    },
+    {
+      "title": "Nome da Foto",
+      "credits": "Alura Space",
+      "image": "/assets/images/gallery/foto-8.png",
+      "id": "8",
+      "tag": "Nebulosas"
+    },
+    {
+      "title": "Nome da Foto",
+      "credits": "Alura Space",
+      "image": "/assets/images/gallery/foto-9.png",
+      "id": "9",
+      "tag": "Estrelas"
+    },
+    {
+      "title": "Nome da Foto",
+      "credits": "Alura Space",
+      "image": "/assets/images/gallery/foto-10.png",
+      "id": "10",
+      "tag": "Estrelas"
+    }
+  ]
+  
+
 
 const Gallery = () => {
     return (
         <section className={styles.gallery}>
             <h2>Navegue pela galeria</h2>
             <Tags />
+            <ul className={styles.gallery__cards}>
+                {galleryImages.map(galleryImage => {
+                    return (
+                         <li key={galleryImage.id}>
+                            <Card 
+                              pictureName={galleryImage.title}
+                              imagePATH={galleryImage.image}
+                              credits={galleryImage.credits}
+                            />
+                        </li>
+                    ) 
+                })}
+            </ul>
         </section>
     )
 }
